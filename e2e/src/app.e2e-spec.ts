ac5c64b0f62007193ed15e80430c0d6f44be3ad5
lvanlowe <lvanlowe@inrvisions.com>
2024-07-26 16:29:34 -0400

    fix: correct app name

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('basic-sample app is running!');
+    expect(page.getTitleText()).toEqual('swa-basic-api app is running!');
   });
 
   afterEach(async () => {
--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false, // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/basic-sample'),
+      dir: require('path').join(__dirname, './coverage/swa-basic-api'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true,
     },
